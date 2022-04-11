7bba8243c8e62b5d081ec03c665876eb5b489494
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-11 11:31:31 +0530

    Add button to clear grid and create new grid based on user input

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
-let html = '';
-for (let i = 0; i < 16; i++) {
-    html += '<div class="row">';
-    for (let j = 0; j < 16; j++) {
-        html += '<div class="box"></div>';
+const grid = (size) => {
+    let html = '';
+    for (let i = 0; i < size; i++) {
+        html += '<div class="row">';
+        for (let j = 0; j < size; j++) {
+            html += '<div class="box"></div>';
+        }
+        html += '</div>';
     }
-    html += '</div>';
-}
 
-container.insertAdjacentHTML('afterbegin', html);
+    container.insertAdjacentHTML('afterbegin', html);
+};
+grid(16);
 
-const boxes = document.querySelectorAll('.box');
+let boxes = document.querySelectorAll('.box');
 boxes.forEach((box) =>
     box.addEventListener('mouseenter', (e) => {
         e.target.classList.add('visited');
@@ -20,5 +23,7 @@ boxes.forEach((box) =>
 );
 
 button.addEventListener('click', () => {
-    boxes.forEach((box) => box.classList.remove('visited'));
+    container.textContent = '';
+    const gridSize = window.prompt('Enter grid size');
+    grid(+gridSize);
 });
